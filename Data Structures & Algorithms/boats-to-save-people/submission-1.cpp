871378c1ea6fa061b0cc7047e3ecb150d8775874
Tran Danh Kiet <128984533+kento-lionheart@users.noreply.github.com>
2026-05-28 05:16:10 +0700

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int res = 0;
+        int l = 0, r = people.size() - 1;
+        while(l <= r){
+            if(people[l] + people[r] <= limit){
+                res++;
+                l++; r--;
+            }
+            else{
+                res++;
+                r--;
+            }
+        }
+        return res;
+    }
+};
